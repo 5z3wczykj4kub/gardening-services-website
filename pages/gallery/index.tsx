569570ccebe5f125723fb9f44f2358f3c6ff53e7
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2022-09-04 12:48:42 +0200

    grab images from cms

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -9,101 +9,43 @@ interface IGalleryProps {
   images: any;
 }
 
-const Gallery: NextPage<IGalleryProps> = ({ images }) => {
-  console.log('images', images);
-
-  return (
-    <>
-      <Head>
-        <title>Website - gallery</title>
-      </Head>
-      <Title className={styles.title} level={4}>
-        Gallery
-      </Title>
-      <Paragraph>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, qui.
-        Tempora, itaque ipsum quae nesciunt in nihil repellat autem iure minima,
-        eaque sunt dolorem dolorum labore similique molestias laudantium sit
-        distinctio neque. Minima cum expedita mollitia, quam laborum voluptate
-        placeat.
-      </Paragraph>
-      <Image.PreviewGroup>
-        <Row className={styles.gallery} gutter={[14, 14]}>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_SANITY_CDN_URL}${
-                images[0].image.asset._ref.split('-')[1]
-              }-840x560.jpg`}
-            />
-          </Col>
-          {/* <Col span={24}>
+const Gallery: NextPage<IGalleryProps> = ({ images }) => (
+  <>
+    <Head>
+      <title>Website - gallery</title>
+    </Head>
+    <Title className={styles.title} level={4}>
+      Gallery
+    </Title>
+    <Paragraph>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, qui.
+      Tempora, itaque ipsum quae nesciunt in nihil repellat autem iure minima,
+      eaque sunt dolorem dolorum labore similique molestias laudantium sit
+      distinctio neque. Minima cum expedita mollitia, quam laborum voluptate
+      placeat.
+    </Paragraph>
+    <Image.PreviewGroup>
+      <Row className={styles.gallery} gutter={[14, 14]}>
+        {images.length === 0 ? (
+          <Col span={24}>
             <Empty description='No images' />
-          </Col> */}
-        </Row>
-      </Image.PreviewGroup>
-    </>
-  );
-};
+          </Col>
+        ) : (
+          images.map(({ imageUrl }: { imageUrl: string }) => (
+            <Col key={imageUrl} xs={24} sm={12} md={8} lg={6}>
+              <Image src={imageUrl} />
+            </Col>
+          ))
+        )}
+      </Row>
+    </Image.PreviewGroup>
+  </>
+);
 
 const getStaticProps = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SANITY_URL!}*[_type == "gallery"]`
+    `${process.env
+      .NEXT_PUBLIC_SANITY_URL!}*[_type == "gallery"]{ "imageUrl": image.asset->url }`
   );
   const { result: images } = await res.json();
 
